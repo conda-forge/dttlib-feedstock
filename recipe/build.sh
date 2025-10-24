f4d1c0871858ea9e3c13281e53d28b39a98046f1
Erik von Reis <erik.vonreis@macbook.dts.ligo-wa.caltech.edu>
2025-10-24 11:52:28 -0700

    More attempts to get mac working

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,11 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-if [[ ${target_platform} =~ .*linux.* ]]; then
-    ln -sf ${PREFIX}/lib/libglib-2.0.so.0 ${PREFIX}/lib/libglib-2.0.so
-fi
-./gen_stub
-$PYTHON -m pip install . -vv
+#if [[ ${target_platform} =~ .*linux.* ]]; then
+#    ln -sf ${PREFIX}/lib/libglib-2.0.so.0 ${PREFIX}/lib/libglib-2.0.so
+#fi
+#./gen_stub
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
