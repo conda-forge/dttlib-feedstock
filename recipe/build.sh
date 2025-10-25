1d45288e415a8c87ae63bc0143c299392dc87321
Erik von Reis <evonreis@caltech.edu>
2025-10-25 15:22:53 -0700

    Adding back in some library paths

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-#export LIBCLANG_PATH=${PREFIX}/lib
+export LIBCLANG_PATH=${PREFIX}/lib
 #export C_INCLUDE_PATH=${PREFIX}/include
 #export CPLUS_INCLUDE_PATH=${PREFIX}/include
-#export LIBRARY_PATH=${PREFIX}/lib:${PREFIX}/lib/python3.12/config-3.12-darwin
-## export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib ${PREFIX}/lib/python3.12/config-3.12-darwin"
+export LIBRARY_PATH=${PREFIX}/lib
+# export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib ${PREFIX}/lib/python3.12/config-3.12-darwin"
 # export RUSTFLAGS="${RUSTFLAGS} -L${PREFIX/lib}"
-echo "***** RUSTFLAGS *****"
-echo ${RUSTFLAGS}
 # find ${PREFIX} -name 'libpython*'
 # For macOS cross-compilation, point to the correct Python library
 # export PYO3_CROSS_LIB_DIR="${PREFIX}/lib"
