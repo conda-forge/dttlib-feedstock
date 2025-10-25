89b2cd2accbddf0eba6009ab79298ce780999b4a
Erik von Reis <evonreis@caltech.edu>
2025-10-24 23:27:50 -0700

    try find libpython

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+echo "***** PYTHON LIBRARY *****"
+echo $(find_libpython)
 export LIBCLANG_PATH=${PREFIX}/lib
 export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export LIBRARY_PATH=${PREFIX}/lib:$(dirname find_libpython)
 # For macOS cross-compilation, point to the correct Python library
 # export PYO3_CROSS_LIB_DIR="${PREFIX}/lib"
 
