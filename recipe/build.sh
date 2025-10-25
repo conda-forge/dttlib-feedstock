101474655835f3cdf4611fc9eeefa9273bd5b193
Erik von Reis <evonreis@caltech.edu>
2025-10-25 14:42:58 -0700

    comment out direct link to python3.12

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,8 @@ export LIBCLANG_PATH=${PREFIX}/lib
 export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib:${PREFIX}/lib/python3.12/config-3.12-darwin
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib ${PREFIX}/lib/python3.12/config-3.12-darwin"
-export RUSTFLAGS="${RUSTFLAGS} -L${PREFIX/lib}"
+# export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib ${PREFIX}/lib/python3.12/config-3.12-darwin"
+# export RUSTFLAGS="${RUSTFLAGS} -L${PREFIX/lib}"
 echo "***** RUSTFLAGS *****"
 echo ${RUSTFLAGS}
 # find ${PREFIX} -name 'libpython*'
