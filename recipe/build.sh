fb29526d422dc64fe5d37e1f119c7e763e276173
Erik von Reis <evonreis@caltech.edu>
2025-10-25 00:02:34 -0700

    try a new idea

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ export LIBCLANG_PATH=${PREFIX}/lib
 export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 # For macOS cross-compilation, point to the correct Python library
 # export PYO3_CROSS_LIB_DIR="${PREFIX}/lib"
 
