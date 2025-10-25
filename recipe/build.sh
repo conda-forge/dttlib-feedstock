52c1827bc7eab5f76f08db0663645561b011daf2
Erik von Reis <evonreis@caltech.edu>
2025-10-24 17:09:04 -0700

    drop some unneeded delete and add in an env variable that might make osx arm builds work

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,9 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
+# For macOS cross-compilation, point to the correct Python library
+export PYO3_CROSS_LIB_DIR="${PREFIX}/lib"
+
 #if [[ ${target_platform} =~ .*linux.* ]]; then
 #    ln -sf ${PREFIX}/lib/libglib-2.0.so.0 ${PREFIX}/lib/libglib-2.0.so
 #fi
@@ -13,6 +16,3 @@ $PYTHON -m pip install . -vv --no-deps --no-build-isolation
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-if [[ ${target_platform} =~ .*linux.* ]]; then
-    rm ${PREFIX}/lib/libglib-2.0.so
-fi
