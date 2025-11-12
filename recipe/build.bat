35af32edbb3fc655dde10d83077a5e8315d23574
Erik von Reis <evonreis@caltech.edu>
2025-11-12 14:25:00 -0800

    fix build.bat

--- recipe/build.bat
+++ recipe/build.bat
@@ -5,7 +5,7 @@ set C_INCLUDE_PATH=%PREFIX%/include
 set CPLUS_INCLUDE_PATH=%PREFIX%/include
 
 
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
