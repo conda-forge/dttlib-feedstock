f6fcb52d0ce9d6976307e7556779713f6d8f7cc6
Erik von Reis <evonreis@caltech.edu>
2025-11-12 16:27:10 -0800

    get rust backtrace

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,6 +4,7 @@ set LIBCLANG_PATH=%PREFIX%/lib
 set C_INCLUDE_PATH=%PREFIX%/include
 set CPLUS_INCLUDE_PATH=%PREFIX%/include
 set PYTHONIOENCODING="utf-8"
+set RUST_BACKTRACE=1
 
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
