36ac5799d86db677dc0c2fc4ae1f810cd7c85a58
Erik von Reis <evonreis@caltech.edu>
2025-11-13 11:03:17 -0800

    more work to get libclang to link on windows.

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,15 +1,15 @@
 
-set LIBCLANG_PATH=%LIBRARY_PREFIX%\bin
+set LIBCLANG_PATH=%LIBRARY_BIN%
 set PYO3_PYTHON=%PYTHON%
-set C_INCLUDE_PATH=%PREFIX%\Library\include
-set CPLUS_INCLUDE_PATH=%PREFIX%\Library\include
+#set C_INCLUDE_PATH=%PREFIX%\Library\include
+#set CPLUS_INCLUDE_PATH=%PREFIX%\Library\include
 set PYTHONIOENCODING="utf-8"
 set RUST_BACKTRACE=1
 
 echo LIBCLANG_PATH=%LIBCLANG_PATH%
 
 echo "++++++++++++++++++"
-dir /s %PREFIX%\*clang*
+dir /s %LIBCLANG_PATH%\*clang*
 
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
